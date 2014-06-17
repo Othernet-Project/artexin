5c5d52bf5631276d6c315d9de6d243e83cc1dce4
Branko Vukelic <branko@brankovukelic.com>
2014-06-17 16:39:32 +0200

    Fixed batch example to work with Python 3

--- examples/fetch_and_zip_batch.py
+++ examples/fetch_and_zip_batch.py
@@ -12,10 +12,11 @@ file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
 import sys
-from os.path import dirname as up, abspath
+from os.path import dirname as up, abspath, join
 import time
 
 sys.path.insert(0, up(up(abspath(__file__))))
+sys.path.insert(0, join(up(up(abspath(__file__))), 'artexin'))
 
 from artexin.batch import batch
 
