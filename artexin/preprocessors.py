eb624616367e3547e555a12a9834df156c206ca0
Branko Vukelic <branko@brankovukelic.com>
2014-07-15 21:43:06 +0200

    Fix __all__ in preprocessors module

--- artexin/preprocessors.py
+++ artexin/preprocessors.py
@@ -15,7 +15,7 @@ from . import __version__ as _version, __author__ as _author
 
 __version__ = _version
 __author__ = _author
-__all__ = ('pp_noop', 'pp_wikipedia',)
+__all__ = ('pp_noop', 'pp_wikipedia', 'pp_fixheaders',)
 
 
 
