56f90ac165c90a946ac8e896b9b5d556f24a0e61
Branko Vukelic <branko@brankovukelic.com>
2014-10-06 16:44:27 +0200

    Added replces key to standalone content packer

--- tools/pack_standalone.py
+++ tools/pack_standalone.py
@@ -28,6 +28,7 @@ from artexin.extract import get_title
 from lib.content_crypto import sign_content
 
 IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif',)
+HTML_EXTENSIONS = ('.html', '.htm',)
 KEYRING = '/var/lib/artexin'
 
 
@@ -48,32 +49,42 @@ def is_image(path):
     return os.path.splitext(path)[1].lower() in IMAGE_EXTENSIONS
 
 
-def count_images(dest):
+def is_html(path):
+    return os.path.splitext(path)[1].lower() in HTML_EXTENSIONS
+
+
+def count_files(dest):
     count = 0
+    images = 0
+    html = 0
     for base_dir, subdirs, files in os.walk(dest):
         for f in files:
             if is_image(f):
+                images += 1
+            elif is_html(f):
+                html += 1
+            else:
                 count += 1
-    return count
+    return count, images, html
 
 
 def get_timestamp(timestamp):
     return timestamp.strftime('%Y-%m-%d %H:%M:%S UTC')
 
 
-def pack_directory(dest, out, url, keyring, key, passphrase,
-                   domain='outernet'):
+def pack_directory(dest, out, url, domain='outernet', replaces=None):
     title = read_title(dest)
     checksum = get_hash(url)
     timestamp = datetime.datetime.now()
 
     # Construct metadata
     metadata = {}
-    metadata['url'] = '%s/%s' % (domain, url)
+    metadata['url'] = url
     metadata['domain'] = domain
     metadata['images'] = count_images(dest)
     metadata['title'] = read_title(dest)
     metadata['timestamp'] = get_timestamp(timestamp)
+    metadata['replaces'] = replaces
 
     # Write metadata
     with open(os.path.join(dest, 'info.json'), 'w', encoding='utf-8') as f:
@@ -87,14 +98,7 @@ def pack_directory(dest, out, url, keyring, key, passphrase,
     # Package the directory
     zippath = os.path.join(out, '%s.zip' % checksum)
     zipdir(zippath, dest)
-    signed = sign_content(zippath, keyring, key, passphrase, output_dir=out)
-    os.unlink(zippath)
-    if not os.path.exists(signed):
-            # Python-gnupg will silently fail. It will log a warning, but won't
-            # raise any exceptions. The only way to know is to test if the file
-            # exists. If the file does not exist, we assume it failed.
-            os.unlink(zippath)
-    return signed, timestamp, metadata, checksum
+    return zippath, timestamp, metadata, checksum
 
 
 if __name__ == '__main__':
@@ -102,15 +106,14 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Package stand-alone content')
-    parser.add_argument('--key', metavar='ID', help='ID of the GnuPG key to '
-                        'use for signing the content', required=True)
+    parser.add_argument('-r', dest='replaces', metavar='MD5', default=None,
+                        help='MD5 ID of content that is being replaced')
     parser.add_argument('source', metavar='INPUT_DIRECTORY',
                         help='input (content) directory')
     parser.add_argument('out', metavar='OUTPUT_PATH', help='outptu directory',
                         default='.')
     args = parser.parse_args()
 
-    passphrase = input('Key passphrase: ')
     url = input('Enter page identifier (leave empty to use directory name): ')
     url = url or os.path.basename(args.source.strip('/'))
     domain = input("Enter domain (leave empty for 'outernet'): ")
@@ -120,20 +123,19 @@ if __name__ == '__main__':
     print('URL: %s' % url)
     print('Domain: %s' % domain)
 
-    signed, timestamp, meta, checksum = pack_directory(args.source, args.out,
-                                                       url, KEYRING, args.key,
-                                                       passphrase, domain)
+    zippath, timestamp, meta, checksum = pack_directory(
+        args.source, args.out, url, domain, args.replaces)
 
-    size = os.stat(signed).st_size
-
+    size = os.stat(zippath).st_size
 
     report_file = 'report-%s.csv' % (timestamp.strftime('%Y-%m-%d_%H-%M'))
 
     with open(report_file, 'w', newline='') as f:
         csv = csv.writer(f, delimiter=',', quotechar='"')
-        csv.writerow(['url', 'size', 'time', 'hash', 'title', 'image'])
+        csv.writerow(['url', 'size', 'time', 'hash', 'title', 'image',
+                      'replaces'])
         csv.writerow([meta['url'], size, 0, checksum, meta['title'],
-                      meta['images']])
+                      meta['images'], meta['replaces']])
 
     print("Finished.")
 
