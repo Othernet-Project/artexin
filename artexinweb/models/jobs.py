3a2ff30dfb6b8b54e67cf5a78fd244e45a141875
Andrean Franc <andrean.franc@gmail.com>
2015-02-12 21:50:47 +0100

    add missing fields to model, make tast targets generic

--- artexinweb/models/jobs.py
+++ artexinweb/models/jobs.py
@@ -11,8 +11,8 @@ MD5_LENGTH = 32
 
 
 class Task(mongoengine.Document):
-    """Tasks are the smallest unit of work, which contain an exact target (URL)
-    that needs to be processed."""
+    """Tasks are the smallest unit of work, which contain an exact target that
+    needs to be processed."""
     QUEUED = "QUEUED"
     PROCESSING = "PROCESSING"
     FAILED = "FAILED"
@@ -31,31 +31,32 @@ class Task(mongoengine.Document):
     job_id = mongoengine.StringField(required=True,
                                      max_length=MD5_LENGTH,
                                      min_length=MD5_LENGTH,
-                                     help_text='ID of parent Job.')
-    url = mongoengine.URLField(required=True,
-                               help_text='Target URL.')
+                                     help_text="ID of parent Job.")
+    target = mongoengine.StringField(required=True,
+                                     help_text="Target URL or filesystem path")
+    domain = mongoengine.StringField(help_text="URL Domain or outernet")
     md5 = mongoengine.StringField(max_length=MD5_LENGTH,
                                   min_length=MD5_LENGTH,
-                                  help_text='MD5 hexdigest of target URL.')
-    title = mongoengine.StringField(help_text='Processed page title.')
+                                  help_text="MD5 hexdigest of target.")
+    title = mongoengine.StringField(help_text="Processed page title.")
     size = mongoengine.IntField(min_value=0,
-                                help_text='Size of page in bytes.')
+                                help_text="Size of page in bytes.")
     images = mongoengine.IntField(min_value=0,
-                                  help_text='Number of images on the page.')
-    timestamp = mongoengine.DateTimeField(help_text='End time of task.')
+                                  help_text="Number of images on the page.")
+    timestamp = mongoengine.DateTimeField(help_text="End time of task.")
     status = mongoengine.StringField(choices=STATUSES,
                                      default=QUEUED,
                                      help_text="Job status.")
 
     @classmethod
-    def create(cls, job_id, url):
-        """Create a new task from the passed in list of URLs.
+    def create(cls, job_id, target):
+        """Create a new task for the passed in target.
 
         :param job_id:  The string ID of the parent job instance
-        :param url:     The target URL that needs to be processed
+        :param target:  The target URL or filesystem path
         :returns:       ``Task`` instance
         """
-        task = cls(job_id=job_id, url=url)
+        task = cls(job_id=job_id, target=target)
         task.save()
         return task
 
@@ -93,6 +94,13 @@ class Job(mongoengine.Document):
         (FINISHED, "Finished"),
     )
 
+    STANDALONE = "STANDALONE"
+    FETCHABLE = "FETCHABLE"
+    JOB_TYPES = (
+        (STANDALONE, "Standalone"),
+        (FETCHABLE, "Fetchable")
+    )
+
     queue_class = rqueue.RedisQueue
 
     job_id = mongoengine.StringField(required=True,
@@ -103,6 +111,9 @@ class Job(mongoengine.Document):
                                      required=True,
                                      default=QUEUED,
                                      help_text="Job status.")
+    job_type = mongoengine.StringField(choices=JOB_TYPES,
+                                       required=True,
+                                       help_text="Job type.")
     scheduled = mongoengine.DateTimeField(required=True,
                                           help_text="Start time of the job.")
     updated = mongoengine.DateTimeField(required=True,
@@ -123,24 +134,24 @@ class Job(mongoengine.Document):
         return md5.hexdigest()
 
     @classmethod
-    def create(cls, urls, **kwargs):
-        """Create a new job from the passed in list of URLs.
+    def create(cls, targets, **kwargs):
+        """Create a new job from the passed in list of target(s).
 
-        :param urls:    Iterable containing URLs to be processed
-        :param kwargs:  All kwargs are stored as additional options of the job
+        :param targets:  Iterable containing URLs or filesystem paths
+        :param kwargs:   All kwargs are stored as additional options of the job
         :returns:       ``Job`` instance
         """
         creation_time = datetime.datetime.utcnow()
 
-        # generate job_id from the current time + the passed in urls
-        job_id = cls.generate_id(creation_time, *list(urls))
+        # generate job_id from the current time + the passed in targets
+        job_id = cls.generate_id(creation_time, *list(targets))
 
         job = cls(job_id=job_id,
                   scheduled=creation_time,
                   updated=creation_time,
                   options=kwargs)
 
-        job.tasks = [Task.create(job_id, url) for url in urls]
+        job.tasks = [Task.create(job_id, t) for t in targets]
         job.save()
         job.schedule()
 
@@ -149,7 +160,7 @@ class Job(mongoengine.Document):
     def schedule(self):
         """Schedule the job for processing by a background worker."""
         queue = self.queue_class()
-        queue.put({'type': 'fetch', 'id': self.job_id})
+        queue.put({'type': self.job_type, 'id': self.job_id})
 
     def retry(self):
         """Retry a previously failed job."""
