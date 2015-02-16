884b814bf1e0fc65389d68bd1ded2ec28965e4cb
Andrean Franc <andrean.franc@gmail.com>
2015-02-16 13:16:36 +0100

    rename templates from plural to singular names. add job_details and job_retry pages

--- artexinweb/controllers/jobs.py
+++ artexinweb/controllers/jobs.py
@@ -2,7 +2,13 @@
 import os
 import uuid
 
-from bottle import request, get, post, jinja2_view, jinja2_template, redirect
+from bottle import (request,
+                    route,
+                    get,
+                    post,
+                    jinja2_view,
+                    jinja2_template,
+                    redirect)
 
 from artexinweb import settings
 from artexinweb.forms import FetchableJobForm, StandaloneJobForm
@@ -10,7 +16,7 @@ from artexinweb.models import Job
 
 
 @get('/jobs/')
-@jinja2_view('jobs.html')
+@jinja2_view('job_list.html')
 def jobs_list():
     status = request.query.get('status')
     if status:
@@ -68,28 +74,47 @@ def jobs_create():
         if form.validate():
             return getattr(JobController, job_type.lower())(job_type, form)
 
-        template_name = 'jobs_{0}.html'.format(job_type.lower())
+        template_name = 'job_{0}.html'.format(job_type.lower())
         return jinja2_template(template_name, form=form, job_type=job_type)
 
-    return jinja2_template('jobs_wizard.html')
+    return jinja2_template('job_wizard.html')
 
 
-@get('/jobs/new/')
+@get('/jobs/actions/new/')
 def jobs_new():
     job_type = request.query.get('type')
     if Job.is_valid_type(job_type):
         form_cls = JobController.forms[job_type]
         form = form_cls()
 
-        return jinja2_template('jobs_{0}.html'.format(job_type.lower()),
+        return jinja2_template('job_{0}.html'.format(job_type.lower()),
                                form=form,
                                job_type=job_type)
 
-    return jinja2_template('jobs_wizard.html')
+    return jinja2_template('job_wizard.html')
+
+
+@route('/jobs/<job_id:re:[a-zA-Z0-9]+>/actions/retry/', method=['GET', 'POST'])
+def jobs_retry(job_id):
+    if request.method == 'POST':
+        job = Job.objects.get(job_id=job_id)
+        if not job.is_finished:
+            job.retry()
+
+        return redirect('/jobs/')
+
+    return jinja2_template('job_retry.html', job_id=job_id)
+
+
+@get('/jobs/<job_id:re:[a-zA-Z0-9]+>/')
+@jinja2_view('job_details.html')
+def jobs_details(job_id):
+    job = Job.objects.get(job_id=job_id)
+    return {'job': job}
 
 
 @get('/jobs/<job_id:re:[a-zA-Z0-9]+>/tasks/')
-@jinja2_view('tasks.html')
+@jinja2_view('task_list.html')
 def task_list(job_id):
     job = Job.objects.get(job_id=job_id)
     return {'task_list': job.tasks, 'job_id': job_id}
