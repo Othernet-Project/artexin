a56b4df5f09c4d4d48ee84979b49f91e9cd7f28b
Andrean Franc <andrean.franc@gmail.com>
2015-03-14 23:31:19 +0100

    refactor zipball creation code into separate function so it can be reused from artexinweb, cover both collect and create_zipball with unittests

--- artexin/pack.py
+++ artexin/pack.py
@@ -8,13 +8,14 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
-import os
-import shutil
-import zipfile
+import copy
+import datetime
 import hashlib
 import logging
+import os
+import shutil
 import tempfile
-import datetime
+import zipfile
 import urllib.parse as urlparse
 
 try:
@@ -30,7 +31,7 @@ from .extract import extract, no_extract, strip_links, process_images
 
 __version__ = _version
 __author__ = _author
-__all__ = ('zipdir', 'collect', 'BASE_DIR')
+__all__ = ('zipdir', 'collect', 'create_zipball', 'BASE_DIR')
 
 
 COMPRESSION = zipfile.ZIP_DEFLATED
@@ -55,6 +56,15 @@ def serialize_datetime(date_obj):
     return date_obj.strftime(TS_FORMAT)
 
 
+def hash_data(*args):
+    md5 = hashlib.md5()
+
+    for data in args:
+        md5.update(bytes(str(data), 'utf-8'))
+
+    return md5.hexdigest()
+
+
 def zipdir(path, dirpath):
     """ Create a zipball at ``path`` containing the directory at ``dirpath``
 
@@ -71,6 +81,77 @@ def zipdir(path, dirpath):
                 cpath = os.path.join(base_dir, path)
                 zipball.write(cpath, os.path.relpath(cpath, basepath))
         zipball.testzip()
+
+
+def create_zipball(src_dir, meta, out_dir, keep_dir=False, keyring=None,
+                   key=None, passphrase=None):
+    """Copies the contents of the passed in `src_dir` to a newly created folder
+    inside `out_dir`. Generates an info.json file from the passed in meta
+    information and writes it into the newly created folder inside `out_dir`.
+    Zip the newly created folder inside `out_dir` and save the zip file into
+    `outdir`.
+    Optionally preserve the newly created folder inside `out_dir`.
+    Optionally encrypt the zip file, replacing it with the signed one.
+
+    :param src_dir:     Source directory where the html and other resources are
+    :param meta:        Meta information to be added to info.json as well
+    :param out_dir:     Path where the zipball will be saved
+    :param keep_dir:    Boolean, if True the newly created folder in `out_dir`
+                        will be preserved
+    :param keyring:     Keyring directory
+    :param key:         Key to use for signing
+    :param passphrase:  Key passphrase
+    """
+    meta = copy.copy(meta)
+
+    checksum = hash_data(meta['url'])
+
+    timestamp = meta['timestamp']
+    meta['timestamp'] = serialize_datetime(timestamp)
+
+    # Create the destination directory
+    dest = os.path.join(out_dir, checksum)
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+    shutil.copytree(src_dir, dest)
+
+    # FIXME: Handle failure
+    meta_path = os.path.join(dest, 'info.json')
+    with open(meta_path, 'w', encoding='utf-8') as meta_file:
+        meta_file.write(json.dumps(meta, indent=2))
+
+    zippath = os.path.join(out_dir, '{0}.zip'.format(checksum))
+    zipdir(zippath, dest)  # FIXME: Handle failure
+
+    if not keep_dir:
+        shutil.rmtree(dest)
+
+    # Encrypt zip file
+    if all([keyring, key, passphrase]):
+        signed = sign_content(zippath,
+                              keyring,
+                              key,
+                              passphrase,
+                              output_dir=out_dir)
+        if not os.path.exists(signed):
+            # Python-gnupg will silently fail. It will log a warning, but won't
+            # raise any exceptions. The only way to know is to test if the file
+            # exists. If the file does not exist, we assume it failed.
+            os.unlink(zippath)
+            meta.update({'timestamp': timestamp,
+                         'error': "Error signing '{0}'".format(zippath)})
+            return meta
+
+        os.unlink(zippath)
+        zippath = signed
+
+    meta.update({'zipfile': zippath,
+                 'size': os.stat(zippath).st_size,
+                 'hash': checksum,
+                 # Pass timestamp as native datetime object
+                 'timestamp': timestamp})
+    return meta
 
 
 def collect(url, keyring=None, key=None, passphrase=None, prep=[], meta={},
@@ -111,22 +192,10 @@ def collect(url, keyring=None, key=None, passphrase=None, prep=[], meta={},
     :param do_extract:  Whether to perform article extraction
     :returns:           Full path of the newly created zipball
     """
-
+    meta = copy.copy(meta)
     # Common metadata
-    meta.update({
-        'url': url,
-        'domain': urlparse.urlparse(url)[1]
-    })
-
-    # Create the destination directory
-    md5 = hashlib.md5()
-    md5.update(bytes(url, 'utf-8'))
-    checksum = md5.hexdigest()
-    dest = os.path.join(base_dir, checksum)
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-    os.mkdir(dest)  # FIXME: Handle failure
-
+    meta.update({'url': url,
+                 'domain': urlparse.urlparse(url).netloc})
     # Fetch and prepare the HTML
     try:
         if javascript:
@@ -139,72 +208,45 @@ def collect(url, keyring=None, key=None, passphrase=None, prep=[], meta={},
         # it will give us a peek into what went down. We will also log the
         # exception just in case.
         logging.exception('Error %s while processing %s' % (err, url))
-        meta.update({
-            'timestamp': datetime.datetime.utcnow(),
-            'error': str(err),
-        })
+        meta.update({'timestamp': datetime.datetime.utcnow(),
+                     'error': str(err)})
+        return meta
 
     timestamp = datetime.datetime.utcnow()
+
     for preprocessor in prep:
         page = preprocessor(page)
+
     if do_extract:
         title, html = extract(page)  # FIXME: Handle failure
     else:
         title, html = no_extract(page)
+
     title = title.strip()
     html = strip_links(html)
 
+    temp_dir = tempfile.mkdtemp()
     # Process images
-    html, images = process_images(html, url, imgdir=dest)
-
-    # Write file metadata
-    meta.update({
-        'timestamp': serialize_datetime(timestamp),
-        'title': title,
-        'images': len(images),
-    })
-    # FIXME: Handle failure
-    with open(os.path.join(dest, 'info.json'), 'w', encoding='utf-8') as f:
-        f.write(json.dumps(meta, indent=2))
-
+    html, images = process_images(html, url, imgdir=temp_dir)
     # Write the HTML file
     # FIXME: Handle failure
-    with open(os.path.join(dest, 'index.html'), 'w', encoding='utf-8') as f:
-        f.write(html)
+    html_path = os.path.join(temp_dir, 'index.html')
+    with open(html_path, 'w', encoding='utf-8') as html_file:
+        html_file.write(html)
 
-    # Create a zip file
-    zippath = os.path.join(base_dir, '%s.zip' % checksum)
-    zipdir(zippath, dest)  # FIXME: Handle failure
+    meta.update({'timestamp': timestamp,
+                 'title': title,
+                 'images': len(images)})
 
-    # Clean-up
-    if not keep_dir:
-        shutil.rmtree(dest)
-
-    stat = os.stat(zippath)
-
-    # Encrypt zip file
-    if all([keyring, key, passphrase]):
-        signed = sign_content(zippath, keyring, key, passphrase,
-                              output_dir=base_dir)
-        if not os.path.exists(signed):
-            # Python-gnupg will silently fail. It will log a warning, but won't
-            # raise any exceptions. The only way to know is to test if the file
-            # exists. If the file does not exist, we assume it failed.
-            os.unlink(zippath)
-            meta.update({
-                'timestamp': datetime.datetime.now(),
-                'error': "Error signing '%s'" % zippath
-            })
-            return meta
-        os.unlink(zippath)
-        zippath = signed
-
-    meta.update({
-        'zipfile': zippath,
-        'size': stat.st_size,
-        'hash': checksum,
-        'timestamp': timestamp,  # Pass timestamp as native datetime object
-    })
+    meta = create_zipball(src_dir=temp_dir,
+                          meta=meta,
+                          out_dir=base_dir,
+                          keep_dir=keep_dir,
+                          keyring=keyring,
+                          key=key,
+                          passphrase=passphrase)
+    # Cleanup
+    shutil.rmtree(temp_dir)
 
     return meta
 
---
+++ artexin/tests/test_pack.py
@@ -0,0 +1,293 @@
+import copy
+import datetime
+import os
+import urllib
+
+from unittest import mock
+
+from ..pack import json, create_zipball, collect, serialize_datetime
+
+
+class TestCreateZipball(object):
+
+    @classmethod
+    def setup_class(cls):
+        cls.meta = {'language': 'en',
+                    'license': 'GFDL',
+                    'url': 'http://en.wikipedia.org/wiki/Outernet',
+                    'timestamp': datetime.datetime.utcnow()}
+        cls.src_dir = '/tmp/some_random_folder'
+        cls.base_out_dir = '/srv/zipballs'
+
+        cls.expected_hash = 'an_md5_hash'
+        cls.expected_size = 1024
+        cls.expected_out_dir = os.path.join(cls.base_out_dir,
+                                            cls.expected_hash)
+        zip_filename = '{0}.zip'.format(cls.expected_hash)
+        cls.expected_zipfile = os.path.join(cls.base_out_dir, zip_filename)
+
+    @mock.patch('artexin.pack.hash_data')
+    @mock.patch('artexin.pack.zipdir')
+    @mock.patch('os.path.exists')
+    @mock.patch('os.stat')
+    @mock.patch('shutil.copytree')
+    @mock.patch('shutil.rmtree')
+    def test_create_zipball_no_crypto(self, shutil_rmtree, shutil_copytree,
+                                      os_stat, os_path_exists, zipdir,
+                                      hash_data):
+        hash_data.return_value = self.expected_hash
+
+        stat_result = mock.Mock(st_size=self.expected_size)
+        os_stat.return_value = stat_result
+        os_path_exists.return_value = True
+
+        expected_meta = copy.copy(self.meta)
+        expected_meta.update({'hash': self.expected_hash,
+                              'size': self.expected_size,
+                              'zipfile': self.expected_zipfile})
+
+        expected_info = copy.copy(self.meta)
+        str_timestamp = serialize_datetime(expected_info['timestamp'])
+        expected_info['timestamp'] = str_timestamp
+        str_expected_info = json.dumps(expected_info, indent=2)
+
+        m_open = mock.mock_open()
+        with mock.patch('builtins.open', m_open):
+            meta = create_zipball(src_dir=self.src_dir,
+                                  meta=self.meta,
+                                  out_dir=self.base_out_dir)
+
+        shutil_copytree.assert_called_once_with(self.src_dir,
+                                                self.expected_out_dir)
+
+        calls = [mock.call(self.expected_out_dir),
+                 mock.call(self.expected_out_dir)]
+        shutil_rmtree.assert_has_calls(calls)
+
+        info_path = os.path.join(self.expected_out_dir, 'info.json')
+        m_open.assert_called_once_with(info_path, 'w', encoding='utf-8')
+
+        file_handle = m_open()
+        file_handle.write.assert_called_once_with(str_expected_info)
+
+        assert meta == expected_meta
+
+    @mock.patch('artexin.pack.sign_content')
+    @mock.patch('artexin.pack.hash_data')
+    @mock.patch('artexin.pack.zipdir')
+    @mock.patch('os.unlink')
+    @mock.patch('os.path.exists')
+    @mock.patch('os.stat')
+    @mock.patch('shutil.copytree')
+    @mock.patch('shutil.rmtree')
+    def test_create_zipball_with_crypto(self, shutil_rmtree, shutil_copytree,
+                                        os_stat, os_path_exists, os_unlink,
+                                        zipdir, hash_data, sign_content):
+        hash_data.return_value = self.expected_hash
+        expected_signed_file = self.expected_zipfile.replace('.zip', '.sig')
+        sign_content.return_value = expected_signed_file
+
+        stat_result = mock.Mock(st_size=self.expected_size)
+        os_stat.return_value = stat_result
+        os_path_exists.return_value = True
+
+        expected_meta = copy.copy(self.meta)
+        expected_meta.update({'hash': self.expected_hash,
+                              'size': self.expected_size,
+                              'zipfile': expected_signed_file})
+
+        expected_info = copy.copy(self.meta)
+        str_timestamp = serialize_datetime(expected_info['timestamp'])
+        expected_info['timestamp'] = str_timestamp
+        str_expected_info = json.dumps(expected_info, indent=2)
+
+        keyring = 'keyring'
+        key = 'key'
+        passphrase = 'passphrase'
+
+        m_open = mock.mock_open()
+        with mock.patch('builtins.open', m_open):
+            meta = create_zipball(src_dir=self.src_dir,
+                                  meta=self.meta,
+                                  out_dir=self.base_out_dir,
+                                  keyring=keyring,
+                                  key=key,
+                                  passphrase=passphrase)
+
+        shutil_copytree.assert_called_once_with(self.src_dir,
+                                                self.expected_out_dir)
+
+        calls = [mock.call(self.expected_out_dir),
+                 mock.call(self.expected_out_dir)]
+        shutil_rmtree.assert_has_calls(calls)
+
+        info_path = os.path.join(self.expected_out_dir, 'info.json')
+        m_open.assert_called_once_with(info_path, 'w', encoding='utf-8')
+
+        file_handle = m_open()
+        file_handle.write.assert_called_once_with(str_expected_info)
+
+        sign_content.assert_called_once_with(self.expected_zipfile,
+                                             keyring,
+                                             key,
+                                             passphrase,
+                                             output_dir=self.base_out_dir)
+
+        os_unlink.assert_called_once_with(self.expected_zipfile)
+
+        assert meta == expected_meta
+
+    @mock.patch('artexin.pack.sign_content')
+    @mock.patch('artexin.pack.hash_data')
+    @mock.patch('artexin.pack.zipdir')
+    @mock.patch('os.unlink')
+    @mock.patch('os.path.exists')
+    @mock.patch('os.stat')
+    @mock.patch('shutil.copytree')
+    @mock.patch('shutil.rmtree')
+    def test_create_zipball_with_crypto_fail(self, shutil_rmtree,
+                                             shutil_copytree, os_stat,
+                                             os_path_exists, os_unlink, zipdir,
+                                             hash_data, sign_content):
+        hash_data.return_value = self.expected_hash
+        expected_signed_file = self.expected_zipfile.replace('.zip', '.sig')
+        sign_content.return_value = expected_signed_file
+
+        stat_result = mock.Mock(st_size=self.expected_size)
+        os_stat.return_value = stat_result
+        os_path_exists.return_value = False
+
+        expected_meta = copy.copy(self.meta)
+        expected_err_msg = "Error signing '{0}'".format(self.expected_zipfile)
+        expected_meta.update({'error': expected_err_msg})
+
+        expected_info = copy.copy(self.meta)
+        str_timestamp = serialize_datetime(expected_info['timestamp'])
+        expected_info['timestamp'] = str_timestamp
+        str_expected_info = json.dumps(expected_info, indent=2)
+
+        keyring = 'keyring'
+        key = 'key'
+        passphrase = 'passphrase'
+
+        m_open = mock.mock_open()
+        with mock.patch('builtins.open', m_open):
+            meta = create_zipball(src_dir=self.src_dir,
+                                  meta=self.meta,
+                                  out_dir=self.base_out_dir,
+                                  keyring=keyring,
+                                  key=key,
+                                  passphrase=passphrase)
+
+        shutil_copytree.assert_called_once_with(self.src_dir,
+                                                self.expected_out_dir)
+
+        shutil_rmtree.assert_called_once_with(self.expected_out_dir)
+
+        info_path = os.path.join(self.expected_out_dir, 'info.json')
+        m_open.assert_called_once_with(info_path, 'w', encoding='utf-8')
+
+        file_handle = m_open()
+        file_handle.write.assert_called_once_with(str_expected_info)
+
+        sign_content.assert_called_once_with(self.expected_zipfile,
+                                             keyring,
+                                             key,
+                                             passphrase,
+                                             output_dir=self.base_out_dir)
+
+        os_unlink.assert_called_once_with(self.expected_zipfile)
+
+        assert meta == expected_meta
+
+
+class TestCollect(object):
+
+    @classmethod
+    def setup_class(cls):
+        cls.meta = {'language': 'en',
+                    'license': 'GFDL'}
+        cls.url = 'http://en.wikipedia.org/wiki/Outernet'
+        cls.timestamp = datetime.datetime.utcnow()
+
+    @mock.patch('artexin.pack.fetch_rendered')
+    def test_collect_fail(self, fetch_rendered):
+        exc = Exception("Bad luck")
+        fetch_rendered.side_effect = exc
+
+        meta = collect(self.url, meta=self.meta, javascript=True)
+
+        expected_meta = copy.copy(self.meta)
+        expected_meta.update({'url': self.url,
+                              'domain': urllib.parse.urlparse(self.url).netloc,
+                              'error': str(exc)})
+
+        assert len(meta) == len(expected_meta) + 1
+
+        for key, value in expected_meta.items():
+            assert meta[key] == value
+
+        assert isinstance(meta['timestamp'], datetime.datetime)
+
+    @mock.patch('shutil.rmtree')
+    @mock.patch('tempfile.mkdtemp')
+    @mock.patch('artexin.pack.create_zipball')
+    @mock.patch('artexin.pack.process_images')
+    @mock.patch('artexin.pack.strip_links')
+    @mock.patch('artexin.pack.extract')
+    @mock.patch('artexin.pack.fetch_rendered')
+    def test_collect(self, fetch_rendered, extract, strip_links,
+                     process_images, create_zipball, tempfile_mkdtemp,
+                     shutil_rmtree):
+        page = 'html page'
+        page_title = 'page title'
+        page_source = 'page source'
+        processed_source = 'processed source'
+        images = range(5)
+        temp_dir = '/tmp/some_folder'
+
+        prep_func = mock.Mock()
+        prep_func.side_effect = lambda x: x
+        fetch_rendered.return_value = page
+        extract.return_value = (page_title, page_source)
+        strip_links.return_value = page_source
+        process_images.return_value = (processed_source, images)
+        tempfile_mkdtemp.return_value = temp_dir
+
+        def mocked_create_zipball(meta, **kwargs):
+            return meta
+        create_zipball.side_effect = mocked_create_zipball
+
+        m_open = mock.mock_open()
+        with mock.patch('builtins.open', m_open):
+            meta = collect(self.url,
+                           meta=self.meta,
+                           javascript=True,
+                           do_extract=True,
+                           prep=[prep_func])
+
+        expected_meta = copy.copy(self.meta)
+        expected_meta.update({'url': self.url,
+                              'domain': urllib.parse.urlparse(self.url).netloc,
+                              'title': page_title,
+                              'images': len(images)})
+
+        assert len(meta) == len(expected_meta) + 1
+
+        for key, value in expected_meta.items():
+            assert meta[key] == value
+
+        assert isinstance(meta['timestamp'], datetime.datetime)
+
+        extract.assert_called_once_with(page)
+        prep_func.assert_called_once_with(page)
+        strip_links.assert_called_once_with(page_source)
+        tempfile_mkdtemp.assert_called_once_with()
+        shutil_rmtree.assert_called_once_with(temp_dir)
+        assert create_zipball.call_count == 1
+
+        html_path = os.path.join(temp_dir, 'index.html')
+        m_open.assert_called_once_with(html_path, 'w', encoding='utf-8')
+
+        file_handle = m_open()
+        file_handle.write.assert_called_once_with(processed_source)
